d8222e8518813b78f51cfbcb151e8ee63ec2452f
Salmaan Saeed <salsaeed999@gmail.com>
2025-10-06 22:16:28 -0400

    rewrote the rust program into a simple job scraping tool

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,27 @@
+use csv::Writer;
 use scraper::Html;
 use scraper::Selector;
-use std::error::Error;
-use std::thread;
-use std::time;
-use thirtyfour::prelude::*;
-use csv::Writer;
-use std::process::Command;
-use std::process::exit;
+use scraper::error::SelectorErrorKind;
+use std::fs::File;
+use std::io::prelude::*;
+use std::process;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
-    let port = "40000";
-    let server_url = format!("http://localhost:{}", port);
-
-    let output = Command::new("sh")
-        .arg("-c")
-        .arg("./chromedriver-binary/chromedriver --version")
-        .output()?;
-
-    let out: String = String::from_utf8(output.stdout)?;
-    let output_list: Vec<&str> = out.split(" ").collect();
-    let version = output_list[1];
-
-    let mut caps = DesiredCapabilities::chrome();
-    let user_agent = format!("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/{} Safari/537.36", version);
-    //caps.add_arg("--headless")?;
-    caps.add_arg(format!("--user-agent={}", user_agent).as_str())?;
-    caps.add_arg("--disable-blink-features=AutomationControlled")?;
-    caps.add_arg("--start-maximized")?;
-    caps.add_experimental_option("excludeSwitches", ["enable-automation"])?;
-    caps.add_experimental_option("useAutomationExtension", false)?;
-
-    let driver = WebDriver::new(server_url.as_str(), caps).await?;
-
+fn main() -> Result<(), SelectorErrorKind<'static>> {
     let indeed_domain = "https://www.indeed.com";
-    let website_url = format!("{indeed_domain}{}", "/jobs?q=help+desk&l=United+States");
-    driver.goto(website_url).await?;
 
-    let dur = time::Duration::from_secs(10);
-    thread::sleep(dur);
-    
-    let title =  driver.title().await?;
-
-    if title == "Just a moment..." {
-        println!("Unable to load web page!");
-        driver.quit().await?;
-        exit(1);
+    let html_file_path = "get_html_content/html_content/index.html";
+    let file_result = File::open(html_file_path);
+    let mut html = String::new();
+    if file_result.is_ok() {
+        let mut file = file_result.unwrap();
+        file.read_to_string(&mut html).unwrap();
+    } else {
+        println!("Unable to read the {} file", html_file_path);
+        process::exit(0x0100);
     }
 
-    let html = driver.source().await?;
-    driver.quit().await?;
-
     let document = Html::parse_document(html.as_str());
-    let job_card_selector = Selector::parse("div.cardOutline").unwrap();
+    let job_card_selector = Selector::parse("div.cardOutline")?;
 
     struct Job {
         title: String,
@@ -63,7 +32,7 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
 
     let mut jobs: Vec<Job> = Vec::new();
     for job_card_element in document.select(&job_card_selector) {
-        let heading_selector = Selector::parse("h2.jobTitle").unwrap();
+        let heading_selector = Selector::parse("h2.jobTitle")?;
         let mut job = Job {
             title: String::new(),
             link: String::new(),
@@ -71,19 +40,19 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
             location: String::new(),
         };
         for heading_element in job_card_element.select(&heading_selector) {
-            let link_selector = Selector::parse("a").unwrap();
+            let link_selector = Selector::parse("a")?;
             for link_element in heading_element.select(&link_selector) {
                 let route = link_element.value().attr("href").unwrap();
                 let link: String = format!("{indeed_domain}{}", route);
                 let job_title = link_element.text().collect::<String>();
-                
+
                 job.title = job_title;
                 job.link = link;
             }
         }
-        let company_selector = Selector::parse("div.company_location").unwrap();
+        let company_selector = Selector::parse("div.company_location")?;
         for company_element in job_card_element.select(&company_selector) {
-            let company_name_selector = Selector::parse("span").unwrap();
+            let company_name_selector = Selector::parse("span")?;
             for company_name_element in company_element.select(&company_name_selector) {
                 if company_name_element.value().attr("data-testid").is_some() {
                     if company_name_element.value().attr("data-testid").unwrap() == "company-name" {
@@ -93,7 +62,7 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
                 }
             }
 
-            let location_selector = Selector::parse("div").unwrap();
+            let location_selector = Selector::parse("div")?;
             for location_element in company_element.select(&location_selector) {
                 if location_element.value().attr("data-testid").is_some() {
                     if location_element.value().attr("data-testid").unwrap() == "text-location" {
@@ -106,18 +75,44 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
         jobs.push(job);
     }
 
-    let mut writer = Writer::from_path("indeed_job_data/jobs.csv")?;
-    writer.write_record(&["Title", "Company_Name", "Location", "Link"])?;
+    let log_file_path = "logs/scrap_jobs.log";
+    let result_logger = File::create(log_file_path);
 
-    for job in jobs {
-        println!("Job Title: {}", job.title);
-        println!("company name: {}", job.company_name);
-        println!("location: {}", job.location);
-        println!("link: {}", job.link);
-        println!("");
+    if result_logger.is_ok() {
+        let mut logger = result_logger.unwrap();
 
-        writer.write_record(&[job.title, job.company_name, job.location, job.link])?;
+        let csv_file_path = "data/indeed_jobs.csv";
+        let writer_result = Writer::from_path(csv_file_path);
+        if writer_result.is_ok() {
+            let mut writer = writer_result.unwrap();
+            writer
+                .write_record(&["Title", "Company_Name", "Location", "Link"])
+                .unwrap();
+
+            for job in jobs {
+                let job_title_info = format!("Job Title: {}\n", job.title);
+                let company_name_info = format!("Company Name: {}\n", job.company_name);
+                let location_info = format!("Location: {}\n", job.location);
+                let link_info = format!("Link: {}\n\n", job.link);
+
+                logger.write(job_title_info.as_bytes()).unwrap();
+                logger.write(company_name_info.as_bytes()).unwrap();
+                logger.write(location_info.as_bytes()).unwrap();
+                logger.write(link_info.as_bytes()).unwrap();
+
+                writer
+                    .write_record(&[job.title, job.company_name, job.location, job.link])
+                    .unwrap();
+            }
+            writer.flush().unwrap();
+        } else {
+            println!("Unable to open the {} file", csv_file_path);
+            process::exit(0x0100);
+        }
+    } else {
+        println!("Unable to open the {} file", log_file_path);
+        process::exit(0x0100);
     }
-    writer.flush()?;
+
     Ok(())
 }
